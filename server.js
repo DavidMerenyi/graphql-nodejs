ff639e5e5cdbc512c731615635d186ffa8898199
Dávid Merényi <peedee4@gmail.com>
2024-09-12 22:13:41 +0200

    graphql schema

--- server.js
+++ server.js
@@ -1,37 +1,70 @@
 var express = require("express")
 var { createHandler } = require("graphql-http/lib/use/express")
-var { buildSchema } = require("graphql")
+var { buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql")
 var { ruruHTML } = require("ruru/server")
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello(name: String!): String
-    age: Int
-    weight: Float
-    isOver18: Boolean
-    hobbies: [String!]!
-  }
-`)
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello(name: String!): String
+//     age: Int
+//     weight: Float
+//     isOver18: Boolean
+//     hobbies: [String!]!
+
+//     user: User
+//   }
+
+//   type User { 
+//     id: Int
+//     name: String
+
+//     posts: [Post]
+//     friends: [User]
+//     invitedBy: User
+//   }
+// `)
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve: () => {
+          return "Hello World"
+        }
+      }
+    }
+  })
+})
 
 // The rootValue provides a resolver function for each API endpoint
-var rootValue = {
-  hello({ name }) {
-    return "Hello " + name
-  },
-  age() {
-    return 30
-  },
-  weight: null,
-  isOver18: true,
-  hobbies() {
-    return ["swimming", "reading"]
-  }
-}
+// var rootValue = {
+//   hello({ name }) {
+//     return "Hello " + name
+//   },
+//   age() {
+//     return 30
+//   },
+
+//   weight: null,
+//   isOver18: true,
+
+//   hobbies() {
+//     return ["swimming", "reading"]
+//   },
+//   user() {
+//     return {
+//       id: 1,
+//       name: "John"
+//     }
+//   }
+// }
 
 const app = express()
 
-app.all('/graphql', createHandler({ schema, rootValue }))
+app.all('/graphql', createHandler({ schema }))
 
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
