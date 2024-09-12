689ab6f33b92687ef807bc7863a6b69b9f022a5a
Dávid Merényi <peedee4@gmail.com>
2024-09-12 22:24:28 +0200

    add yoga

--- server.js
+++ server.js
@@ -1,71 +1,25 @@
-var express = require("express")
-var { createHandler } = require("graphql-http/lib/use/express")
-var { buildSchema, GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql")
-var { ruruHTML } = require("ruru/server")
+import express from 'express'
+import { ruruHTML } from 'ruru/server'
 
-// // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello(name: String!): String
-//     age: Int
-//     weight: Float
-//     isOver18: Boolean
-//     hobbies: [String!]!
+import { createYoga, createSchema } from 'graphql-yoga'
 
-//     user: User
-//   }
-
-//   type User { 
-//     id: Int
-//     name: String
-
-//     posts: [Post]
-//     friends: [User]
-//     invitedBy: User
-//   }
-// `)
-
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => {
-          return "Hello World"
-        }
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => 'Hello from Yoga!'
       }
     }
   })
 })
 
-// The rootValue provides a resolver function for each API endpoint
-// var rootValue = {
-//   hello({ name }) {
-//     return "Hello " + name
-//   },
-//   age() {
-//     return 30
-//   },
-
-//   weight: null,
-//   isOver18: true,
-
-//   hobbies() {
-//     return ["swimming", "reading"]
-//   },
-//   user() {
-//     return {
-//       id: 1,
-//       name: "John"
-//     }
-//   }
-// }
-
 const app = express()
-
-app.all('/graphql', createHandler({ schema }))
-
+app.all('/graphql', yoga)
 // Serve the GraphiQL IDE.
 app.get("/", (_req, res) => {
   res.type("html")
