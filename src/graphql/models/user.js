9b0efd0978119e85279b17ceff57684385da6d2e
Dávid Merényi <peedee4@gmail.com>
2024-09-26 20:10:33 +0200

    crud operation on user

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,19 +1,29 @@
+import { update } from 'lodash'
+import { ObjectId } from 'mongodb'
+
 export const typeDef = /* GraphQL */ `
-    type Query {    
+    type Query {  
+        users: [User!]!
         user: User
     }
 
     type Mutation {
         createUser(user: NewUserInput!): User
+        deleteUser(id: ID!): Bolean
+        updateUser(id: ID!, user: UpdateUserInput!): User
     }
 
     input NewUserInput {
         name: String!
-        age: Int!
+        email: String!
     }
 
+
+    input UpdateUserInput {    
+        email: String!
+    }
     type User {
-        id: Int
+        id: ID!
         name: String
         age: Int
     }
@@ -21,22 +31,40 @@ export const typeDef = /* GraphQL */ `
 
 export const resolvers = {
     Query: {
-        user: () => ({ id: 1, name: 'David' }),
+        users: (obj, args, { mongo }) => {
+            return mongo.users.find().limit(20).toArray()
+        },
+        user: (obj, { id }, { mongo }) => {
+            return mongo.users.findOne({ _id: new ObjectId(String(id)) })
+        }
     },
 
     Mutation: {
         createUser: async (_, { user }, { mongo }) => {
-            const movies = await mongo.movies.find().toArray()
-            console.log(movies)
+            const response = await mongo.users.insertOne(user)
 
             return {
-                id: 1,
+                id: response.insertedId,
                 ...user
             }
-        }
-    },
+        },
+        deleteUser: async (_, { id }, { mongo }) => {
+            const response = await mongo.users.deleteOne({ _id: new ObjectId(String(id)) })
 
-    User: {
-        name: (obj) => obj.name.trim().toUpperCase()
+            return response.deletedCount === 1
+        },
+        updateUser: async (_, { id, user }, { mongo }) => {
+            const response = await mongo.users.findOneAndUpdate(
+                { _id: new ObjectId(String(id)) },
+                { $set: { "email": user.email } },
+                { returnOriginal: false }
+            )
+
+            return response.value
+        },
+
+        User: {
+            id: ({ _id, id }) => _id || id,
+            name: (obj) => obj.name.trim().toUpperCase()
+        }
     }
-}
